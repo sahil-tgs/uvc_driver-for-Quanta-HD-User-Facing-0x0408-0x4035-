cf55fa9e7c309e9039e71c17ed68b9df7f5a7b0c
sahil-tgs <sahiltgs@gmail.com>
2024-08-31 12:27:03 +0530

    arch-script

--- compile_module.sh
+++ compile_module.sh
@@ -1,33 +1,47 @@
-#!/bin/bash 
-#Test linux distribution and version
-source /etc/os-release
-if [ "$ID" != "ubuntu" ]; then 
-	echo "Sorry, this script works only for Ubuntu distribution"
-	exit
+#!/bin/bash
+
+# Test if the system is Arch Linux
+if [ ! -f /etc/arch-release ]; then
+    echo "Sorry, this script works only for Arch Linux"
+    exit 1
 fi
 
-if [ "$VERSION_ID" != "22.04" ]; then 
-	echo "Sorry, this script works only for Ubuntu 22.04 LTS"
-	exit
-fi
+# Update package list and upgrade packages
+sudo pacman -Syu --noconfirm
 
+# Install tools needed for module compilation
+sudo pacman -S --noconfirm base-devel linux-headers
 
-sudo apt update # update package list
-sudo apt upgrade # upgrade packages
-sudo apt install build-essential -y # install tools needed for module compilation
-
-#get driver code to compile a patch it
+# Get driver code to compile and patch it
 cd ~  # change to your home directory
-apt-get source linux-modules-extra-$(uname -r)  #download in your home, the kernel source file version that match your used kernel
-cd ~/linux-*/drivers/media/usb/uvc # change to the currently created uvc directory
-mv uvc_driver.c uvc_driver.old  # rename/backup the uvc driver soruce file, that need to be updated
-wget https://raw.githubusercontent.com/Giuliano69/uvc_driver-for-Quanta-HD-User-Facing-0x0408-0x4035-/main/uvc_driver.c # download the updated driver source file
 
-#compile and install
-make -j4 -C /lib/modules/$(uname -r)/build M=$(pwd) modules  # complie the updated video modules for your kernel version
-sudo cp uvcvideo.ko /lib/modules/$(uname -r)/kernel/drivers/media/usb/uvc/  #install the video driver module in the system
-sudo rmmod uvcvideo && sudo modprobe uvcvideo
-#reboot  #reboot to check your camera is working
+# Download the kernel source
+sudo pacman -S --noconfirm asp
+asp checkout linux
+cd linux/trunk
 
+# Extract the source
+makepkg -o
 
+# Navigate to the UVC driver directory
+cd src/linux-*/drivers/media/usb/uvc
 
+# Backup the original driver file
+mv uvc_driver.c uvc_driver.old
+
+# Download the updated driver source file
+wget https://raw.githubusercontent.com/Giuliano69/uvc_driver-for-Quanta-HD-User-Facing-0x0408-0x4035-/main/uvc_driver.c
+
+# Compile and install
+make -j$(nproc) -C /lib/modules/$(uname -r)/build M=$(pwd) modules
+
+# Install the video driver module in the system
+sudo install -Dm644 uvcvideo.ko /lib/modules/$(uname -r)/kernel/drivers/media/usb/uvc/
+
+# Update module dependencies
+sudo depmod -a
+
+# Unload and reload the module
+sudo modprobe -r uvcvideo && sudo modprobe uvcvideo
+
+echo "Driver updated. You may need to reboot for changes to take effect."
